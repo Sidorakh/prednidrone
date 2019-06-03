2af9ab9e182fd82d390d0001f631e4bd17b70f06
Andrew Rutherford <Sidorakh@users.noreply.github.com>
2019-06-03 15:57:36 +1000

    Spoonbank + gitignore

---
+++ cmd/badrheumy.js
@@ -0,0 +1,8 @@
+module.exports.description = {
+    name:"badrheumy",
+    description:"Complain about a bad rheumatologist",
+    usage: "`!badrheumy"
+}
+module.exports.call = async(client,global,msg,args) => {
+    return 'How do you even fuck that up!';
+}
