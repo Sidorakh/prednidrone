69a2f720f6a620957f787d390e817dfa480b34c0
Andrew Rutherford <sentrakh@gmail.com>
2019-09-25 22:28:41 +1000

    FUCKFUCKFUCKFUCK MISSED RETURN HOW

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ client.on('error',console.error);
 client.on('message',async(msg)=>{
     if (msg.content[0] != config.prefix) {
         shallow.buzz(msg.content);
+        return;
     }
     let str = msg.content.substr(1);
     let [cmd,...args] = str.split(' ');
