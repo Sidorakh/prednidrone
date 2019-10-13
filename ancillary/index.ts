09a59f9fce25006e477ccbfc00697522660e99bb
Andrew Rutherford <sentrakh@gmail.com>
2019-10-13 22:38:00 +1100

    More additions

--- ancillary/index.ts
+++ ancillary/index.ts
@@ -2,15 +2,18 @@ import {ShallowsServices} from './ss';
 import {DatabaseHelper} from '../database-helper'
 import * as express from 'express';
 import * as discord from 'discord.js';
+import {ServerHandler} from './server';
 
 export class Services {
     ss: ShallowsServices;
     dbh: DatabaseHelper;
-    app: express.Application;
+    app: ServerHandler;
+    list_roles: (id: string) => string;     // function definition
 
-    constructor(dbh: DatabaseHelper) {
+    constructor(dbh: DatabaseHelper, list_roles: (id: string)=>string[], port:string) {
         this.dbh = dbh;
         this.ss = new ShallowsServices(this.dbh);
+        this.app = new ServerHandler(list_roles, port);
     }
 
     public buzz(msg: String) {
--- ancillary/server.ts
+++ ancillary/server.ts
@@ -15,66 +15,90 @@ const page = (_page: (dbh: DatabaseHelper, req:express.Request, res:express.Resp
     }
 }
 
+export class ServerHandler {
+    private app: express.Application;
+    constructor( list_roles: (id: string)=>string[], port: string) {
+        // Passport
+        const auth_strategy = new discord_strategy ({
+                clientID:process.env.CLIENT_ID,
+                clientSecret:process.env.SECRET_ID,
+                callbackURL:process.env.CALLBACK_URL
+            },
+            (access_token,refresh_token,profile,cb)=>{
+                //@ts-ignore
+                profile.refresh_token = refresh_token; 
+                //@ts-ignore
+                profile.access_token = access_token;
+                const avatar_base = 'https://cdn.discordapp.com';
+                let avatar_url = `${avatar_base}`;
+                if (profile.avatar) {
+                    avatar_url += `/avatars/${profile.id}/${profile.avatar}.${(profile.avatar.includes('a_')?'gif':'png')}`
+                } else {
+                    avatar_url += `/embed/avatars/${parseInt(profile.discriminator) % 5}.png`;
+                }
+                //@ts-ignore
+                profile.avatar_url = avatar_url;
+                cb(null,profile);
+            }
+        );
 
-// Passport
+        passport.use(auth_strategy);
+        passport.serializeUser((user:any,done)=>{
+            active_sessions[user.id] = user;
+            done(null,user);
+        });
+        passport.deserializeUser((user:any,done)=>{
+            done(null,active_sessions[user.id]);
+        })
 
-const auth_strategy = new discord_strategy ({
-        clientID:process.env.CLIENT_ID,
-        clientSecret:process.env.SECRET_ID,
-        callbackURL:process.env.CALLBACK_URL
-    },
-    (access_token,refresh_token,profile,cb)=>{
-        //@ts-ignore        -- need to store these with profile in session
-        profile.refresh_token = refresh_token; 
-        //@ts-ignore        -- need to store these with profile in session
-        profile.access_token = access_token;
-        const avatar_base = 'https://cdn.discordapp.com';
-        let avatar_url = `${avatar_base}`;
-        if (profile.avatar) {
-            avatar_url += `/avatars/${profile.id}/${profile.avatar}.${(profile.avatar.includes('a_')?'gif':'png')}`
-        } else {
-            avatar_url += `/embed/avatars/${parseInt(profile.discriminator) % 5}.png`;
-        }
-        //@ts-ignore        -- need to store these with profile in session
-        profile.avatar_url = avatar_url;
-        cb(null,profile);
+
+
+        const app: express.Application = express();
+        app.use(cookie_parser());
+        app.use(body_parser.json());
+        app.use(body_parser.urlencoded({extended:false}));
+        app.use(session({secret:process.env.EXPRESS_SECRET,resave:true,saveUninitialized:true}));
+        app.use(passport.initialize());
+        app.use(passport.session());
+        app.use('/assets',express.static('./assets'));
+        app.set('view engine', 'ejs');
+
+
+
+
+
+
+        // Setup
+        app.get('/',(req,res,next)=>{
+            if (req.user) {
+                //@ts-ignore
+                const user_roles: string = list_roles(req.user.id);
+                let role_id = 0;
+                if (user_roles.indexOf('Channel Host') > 0) {
+                    role_id = 1;
+                }
+                if (user_roles.indexOf('Chat Mod') > 0) {
+                    role_id = 2;
+                }
+                if (user_roles.indexOf('Admins') > 0) {
+                    role_id = 3;
+                }
+                
+            }   
+            next();
+        })
+
+        // Unauthroized allowed
+
+
+        app.get('/',page(pages.home));
+
+        // Authorization required
+
+
+        app.listen(port);
     }
-);
-
-passport.use(auth_strategy);
-passport.serializeUser((user:any,done)=>{
-    active_sessions[user.id] = user;
-    done(null,user);
-});
-passport.deserializeUser((user:any,done)=>{
-    done(null,active_sessions[user.id]);
-})
-
-
-
-export const app: express.Application = express();
-app.use(cookie_parser());
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({extended:false}));
-app.use(session({secret:process.env.EXPRESS_SECRET,resave:true,saveUninitialized:true}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/assets',express.static('./assets'));
-app.set('view engine', 'ejs');
-
-
-
-
-
-
-
-
-// Unauthroized allowed
-app.get('/',page(pages.home));
-
-
-
-
-
-// Authorization required
-
+    public get_app(): express.Application {
+        return this.app;
+    }
+}
